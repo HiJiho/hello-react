481105499b65aa39a9158e658fd8293c9e5196e5
김지호 <jiho001108@gmail.com>
2024-04-10 21:56:06 +0900

    onChange 이벤트 핸들링하기

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-// import Say from './Say';
-import Counter from "./Counter";
+import EventPractice from './EventPractice';
 
 const App = () => {
-  return <Counter />;
+  return <EventPractice />;
 };
 
 export default App;
